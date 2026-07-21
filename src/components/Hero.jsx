6e1f87545135232ce149e480fe211a576f4b2073
Kwazzota <ryabukhin2512@gmail.com>
2026-07-21 20:46:37 +0500

    trying hpw fast will change on the internet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
         <div className="hero">
             <div className="hero__body">
-                <h1 className="hero__title">Kwarz</h1>
+                <h1 className="hero__title">Snt Kwarz</h1>
                 <div className="hero__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ea non nostrum quae recusandae, soluta ullam vero voluptatibus. Accusamus asperiores atque consectetur, eius eveniet laudantium maiores maxime nam omnis quidem?</div>
             </div>
         </div>
